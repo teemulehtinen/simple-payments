5d38364ef2243d9e0483ce47a74ec0a54d2eec28
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2015-12-01 12:43:10 +0200

    Setup environment to run old Django 1.4 project

--- settings.py
+++ settings.py
@@ -87,7 +87,7 @@ TEMPLATE_DIRS = (
 
 MEDIA_URL='/static/'
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.debug',
     'django.core.context_processors.media',
